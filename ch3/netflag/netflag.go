7d8ccfa9eaef7186487397d9a0b94047bbc77b00
Julien Chien <jchien@quantcast.com>
2016-06-18 19:01:18 -0700

    netflag p2

--- ch3/netflag/netflag.go
+++ ch3/netflag/netflag.go
@@ -5,19 +5,24 @@ import "fmt"
 type Flags uint
 
 const (
-	FlagUp Flags = 1 << iota // is up
-	FlagBroadcast		 // supports boradcast access capability
-	FlagLoopback		// is a loopback interface
-	FlagPointToPoint	// belongs to a point-to-point link
-	FlagMulticast		// supports multicast access capability
+	FlagUp           Flags = 1 << iota // is up
+	FlagBroadcast                      // supports boradcast access capability
+	FlagLoopback                       // is a loopback interface
+	FlagPointToPoint                   // belongs to a point-to-point link
+	FlagMulticast                      // supports multicast access capability
 )
 
-func IsUp(v Flags) bool	{ return v&FlagUp == FlagUp}
-func TurnDown(v *Flags)	{ *v &^= FlagUp}
-func SetBroadcast(v *Flags) { *v |= FlagBroadcast}
-func IsCast(v Flags) bool { return v&(FlagBroadcast|FlagMulticast) != 0}
+func IsUp(v Flags) bool     { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)     { *v &^= FlagUp }
+func SetBroadcast(v *Flags) { *v |= FlagBroadcast }
+func IsCast(v Flags) bool   { return v&(FlagBroadcast|FlagMulticast) != 0 }
 
 func main() {
 	var v Flags = FlagMulticast | FlagUp
-	fmt.Printf("%b %t\
-	
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	TurnDown(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	SetBroadcast(&v)
+	fmt.Printf("%b %t\n", v, IsUp(v))
+	fmt.Printf("%b %t\n", v, IsCast(v))
+}
